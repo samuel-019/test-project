99461907cdfef6382becdb0dfe4fc35ab43819c9
samuel-019 <65654342+samuel-019@users.noreply.github.com>
2020-11-20 17:13:03 +0530

    Update projectWizardConnectivity.js

--- pages/projectWizardConnectivity.js
+++ pages/projectWizardConnectivity.js
@@ -1,9 +1,17 @@
 var projectWizardConnectivity = {
 	
  clickOnNextCtaConnectivity: function(){
-  this.waitForElementVisible('@clickOnNextButtonConnectivity', 10000, 'Project step 4 Form Displayed')
+  this.waitForElementVisible('@clickOnNextButtonConnectivity', 10000)
   return this.click('@clickOnNextButtonConnectivity')
- }
+ },
+ enterUniqueId:  function () {
+    // date to set in project name
+    var date = new Date()
+    var MailchimpId = 'TestId ' + date;
+    this.waitForElementVisible('@UniqueAudienceIdField', 10000)
+    this.setValue('@UniqueAudienceIdField',MailchimpId)
+    return MailchimpId
+  },
 
 };
 
@@ -11,12 +19,16 @@ module.exports = {
   commands: [projectWizardConnectivity],
   url: function () {
     this.api.maximizeWindow()
-    return this.api.launchUrl + 'user/project/options'
+    return this.api.launchUrl + 'user/project/connectivity'
   },
 
   elements:{
   	clickOnNextButtonConnectivity: {
-      selector: '#vertical-wizard-pane-options button:nth-child(2)'
-    }
+      selector: '#vertical-wizard-pane-connectivity button:nth-child(2)'
+    },
+    UniqueAudienceIdField: {
+      selector: '#stripe-tokenizer-key'
+    },
+
   }
   };
