598532e9ab7ef389a3d18a152f5e5d2cd8ac70bf
samuel-019 <65654342+samuel-019@users.noreply.github.com>
2020-11-20 17:14:18 +0530

    Update projectWizardSummary.js

--- pages/projectWizardSummary.js
+++ pages/projectWizardSummary.js
@@ -2,8 +2,12 @@
 var projectWizardSummary = {
 	
  clickOnActivateButton: function(){
-  this.waitForElementVisible('@clickOnActivate', 10000, 'Project was activated')
+  this.waitForElementVisible('@clickOnActivate', 10000)
   return this.click('@clickOnActivate')
+ },
+ clickOnSaveAndExitButton: function(){
+  this.waitForElementVisible('@clickOnSaveAndExit', 10000)
+  return this.click('@clickOnSaveAndExit')
  }
 
 };
@@ -18,6 +22,10 @@ module.exports = {
   elements:{
   	clickOnActivate: {
       selector: '#vertical-wizard-pane-summary .btn-group button:nth-child(1)'
-    }
+    },
+    clickOnSaveAndExit:{
+      selector: '#vertical-wizard-pane-summary .pull-right'
+    },
+
   }
   };
