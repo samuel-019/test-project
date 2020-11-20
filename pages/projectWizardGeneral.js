25171735b0e068d8b3e19b3c3d672b30b750d958
samuel-019 <65654342+samuel-019@users.noreply.github.com>
2020-11-20 17:11:02 +0530

    Update projectWizardGeneral.js

--- pages/projectWizardGeneral.js
+++ pages/projectWizardGeneral.js
@@ -11,13 +11,92 @@ var projectWizardGeneral = {
   enterDescription: function () {
     this.waitForElementVisible('@projectDescriptionField',10000)
     return this.setValue('@projectDescriptionField','Sample Project Please Ignore it')
+  
+
   },
 
   clickNext: function () {
-    this.waitForElementVisible('@nextButton', 10000,'project step 3 Form Displayed')
+    this.waitForElementVisible('@nextButton', 10000)
     return this.click('@nextButton')
   },
+  enterLocation: function () {
+    this.waitForElementVisible('@location',10000)
+    return this.setValue('@location','New York')
+  },
+  enterDateAndTime: function () {
+    var date = new Date()
+    this.waitForElementVisible('@dateAndTime',10000)
+    return this.setValue('@dateAndTime',date)
+  },
+  enterPrice: function () {
+    this.waitForElementVisible('@eventPrice',10000)
+    this.click('@eventPrice')
+    return this.setValue('@eventPrice','20.00')
+  },
+  enterMonthlyMembershipPrice: function(){
+    this.waitForElementVisible('@monthlyMembershipPrice',10000)
+    return this.setValue('@monthlyMembershipPrice','50')
+  },
+  enterQuarterlyMembershipPrice: function(){
+    this.waitForElementVisible('@quarterlyMenmbershipPrice',10000)
+    return this.setValue('@quarterlyMenmbershipPrice','100')
+  },
+  enterYearlyMembershipPrice: function(){
+    this.waitForElementVisible('@yearlyMembershipPrice',10000)
+    return this.setValue('@yearlyMembershipPrice','400')
+  },
 
+  fillFormAndSubmit: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.click('#project-min-payment-amount')
+    this.clickNext()
+  },
+  fillFormAndSubmit2: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.click('.col-sm-6 .btn')
+    this.click('#project-thank-you')
+    this.clickNext()
+  },
+  fillFormAndSubmitForEvents: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.enterLocation()
+    this.enterDateAndTime()
+    this.click('.col-sm-6 .btn')
+    this.click('#project-thank-you')
+    this.clickNext()
+  },
+  fillFormAndSubmitForEventsWithPayment: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.enterLocation()
+    this.enterDateAndTime()
+    this.enterPrice()
+    this.click('.col-sm-6 .btn')
+    this.click('#project-thank-you')
+    this.clickNext()
+  },
+  fillFormAndSubmitForMembershipProject: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.enterMonthlyMembershipPrice()
+    this.enterQuarterlyMembershipPrice()
+    this.enterYearlyMembershipPrice()
+    this.click('.col-sm-6 .btn')
+    this.click('#project-thank-you')
+    this.clickNext()
+  },
+  fillFormAndSubmitForVoucherProject: function () {
+    this.enterProjectName()
+    this.enterDescription()
+    this.enterPrice()
+    this.click('.col-sm-6 .btn')
+    this.click('#project-thank-you')
+    this.clickNext()
+  },
+  
 };
 
 module.exports = {
@@ -40,6 +119,27 @@ module.exports = {
   	},
   	nextButton: {
       selector: '#vertical-wizard-pane-description  .pull-right'
+    },
+    bodyClick:{
+    	selector: '#project-min-payment-amount'
+    },
+    location:{
+      selector: '#project-location'
+    },
+    dateAndTime:{
+      selector: '.col-sm-8 #project-name'
+    },
+    eventPrice:{
+      selector: '#project-payment-amount'
+    },
+    monthlyMembershipPrice: {
+      selector: '#project-monthly-membership-price'
+    },
+    quarterlyMenmbershipPrice:{
+      selector: '#project-quarterly-membership-price'
+    },
+    yearlyMembershipPrice: {
+      selector: '#project-yearly-membership-price'
     }
 
    }
